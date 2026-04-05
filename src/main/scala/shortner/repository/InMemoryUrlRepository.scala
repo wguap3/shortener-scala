29278cbe2a9c440e121a901cddd620f64a7856c2
3wguap <rnbrell@yandex.by>
2026-04-05 15:06:13 +0400

    add repo(class)

---
+++ src/main/scala/shortner/repository/InMemoryUrlRepository.scala
@@ -0,0 +1,27 @@
+package shortner.repository
+
+import shortner.model.Url
+import zio.{Task, ZIO}
+
+import java.util.concurrent.ConcurrentHashMap
+
+class InMemoryUrlRepository extends UrlRepository{
+  private val storage = new ConcurrentHashMap[String,Url]()
+
+  override def save(url: Url): Task[Url] =
+    ZIO.attempt{
+      storage.put(url.shortCode,url)
+      url
+    }
+
+  override def findByCode(shortCode: String): Task[Option[Url]] =
+    ZIO.attempt{
+      Option(storage.get(shortCode))
+    }
+
+  override def incrementClick(shortCode: String): Task[Unit] =
+    ZIO.attempt{
+      storage.computeIfPresent(shortCode,(_,url) => url.copy(clickCount = url.clickCount + 1))
+    }
+}
+
